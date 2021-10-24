cf6796f0c2d241986181985f6d072f8c0ff0398d
John Hicks <jhick046@odu.edu>
2021-10-24 15:15:41 -0400

    Incorporates demo Jflex build
    This does not fulfill the requirements for
    lexing C++. It is a test of the build setup.

---
+++ src/main/java/edu/odu/cs/cs350/dupedetector/TokenKinds.java
@@ -0,0 +1,12 @@
+package edu.odu.cs.cs350.dupedetector;
+
+/**
+ * All possible token types.
+ * 
+ * @author zeil, original
+ *
+ */
+public enum TokenKinds {
+    KEYWORD, INTEGER_LITERAL, IDENTIFIER, EOF};
+
+// end code copied and adapted from zeil.
