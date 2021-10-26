427bf7aef960b8acf8fa2f3b469d333180f22463
cs_jmcfa001 (Jacob McFadden) <jmcfa001@odu.edu>
2021-10-26 07:42:42 -0400

    Started compiling some functions for the report class

--- src/main/java/edu/odu/cs/cs350/dupedetector/Report.java
+++ src/main/java/edu/odu/cs/cs350/dupedetector/Report.java
@@ -1,6 +1,7 @@
 package edu.odu.cs.cs350.dupedetector;
 
-/* The report is where information is compiled together and printed out. It is
+/**
+ *  The report is where information is compiled together and printed out. It is
  * divided into 3 primary sections of: 
  * 
  * Source files read
@@ -12,5 +13,36 @@ package edu.odu.cs.cs350.dupedetector;
 
 public class Report {
 
-
+	/**
+	 * Creates a report object
+	 * 
+	 * @param exampleNumber
+	 */
+	public Report() {
+		
+	}
+	
+	/**
+	 * Prints the entire report for the program when called.
+	 * 
+	 * Wrapper for more specific print functions.
+	 */
+	public void printReport() {
+		printRefactoringReport();
+	}
+	
+	/**
+	 * Organizes and prints a the refactoring suggestions part of the
+	 * report.
+	 */
+	private void printRefactoringReport() {
+		
+	}
+	
+	/**
+	 * Sorts the refactorings from greatest opportunity to least opportunity
+	 */
+	private void sortRefactorings() {
+		
+	}	
 }
