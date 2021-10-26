0eb2761d71acb71fae532c3abd94122c485e2af2
cs_jmcfa001 (Jacob McFadden) <jmcfa001@odu.edu>
2021-10-26 02:52:54 -0400

    Gradle update to allow building even if unit test fail + report comment

--- src/main/java/edu/odu/cs/cs350/dupedetector/Report.java
+++ src/main/java/edu/odu/cs/cs350/dupedetector/Report.java
@@ -1,5 +1,16 @@
 package edu.odu.cs.cs350.dupedetector;
 
+/* The report is where information is compiled together and printed out. It is
+ * divided into 3 primary sections of: 
+ * 
+ * Source files read
+ * Suggested refactorings
+ * Possible number of refactorings
+ * 
+ * @author Jacob McFadden: created the suggested refactorings output
+ */
+
 public class Report {
+
 
 }
