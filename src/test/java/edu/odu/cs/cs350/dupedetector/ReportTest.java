a677b71afb0c63b0f24289ac539a06d9dbeaf5c9
cs_jmcfa001 (Jacob McFadden) <jmcfa001@odu.edu>
2021-10-26 02:33:03 -0400

    Made changes to Gradle to allow Hamcrest. Created empty Report class

---
+++ src/test/java/edu/odu/cs/cs350/dupedetector/ReportTest.java
@@ -0,0 +1,25 @@
+package edu.odu.cs.cs350.dupedetector;
+
+//Junit Imports
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+//Wrappers for JUnit that make it more readable
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+class ReportTest {
+
+	//Dummy JUnit Test
+	@Test
+	void test() {
+		fail("Not yet implemented");
+		
+	}
+	//Dummy Hamcrest Test
+	@Test
+	void hamcrestTest() {
+		assertThat("Not yet implemented", false);
+	}
+
+}
