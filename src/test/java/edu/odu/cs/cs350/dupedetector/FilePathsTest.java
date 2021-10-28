cc46171d6b0c9e90b716a761a3cd6765d6488cd1
cs_irepl001 (Isaac Replogle) <irepl001@odu.edu>
2021-10-27 22:57:31 -0400

    Added FilePathsTest For Testing Purposes

---
+++ src/test/java/edu/odu/cs/cs350/dupedetector/FilePathsTest.java
@@ -0,0 +1,13 @@
+package edu.odu.cs.cs350.dupedetector;
+
+//Junit Imports
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+//Wrappers for JUnit that make it more readable
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+class FilePathsTest {
+
+}
