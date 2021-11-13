296d52b6bd9b4d33c0267d4684a5d0b8d68bbd96
cs_jmcfa001 (Jacob McFadden) <jmcfa001@odu.edu>
2021-11-13 03:11:02 -0500

    Creates Test Class to insert Integration Tests

---
+++ src/test/java/edu/odu/cs/cs350/dupedetector/IntegrationTest.java
@@ -0,0 +1,21 @@
+package edu.odu.cs.cs350.dupedetector;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Location where we can compile the tests that interact with multiple systems.
+ * Some of these tests may perform to the output, others using standard assertions.
+ * 
+ * Refer to comment above method for which.
+ * 
+ * @author Jacob
+ */
+
+class IntegrationTest {
+	
+	@Test 
+	void testSupplyRefactorSuggestionPropertyValues() {
+		
+	}
+}
