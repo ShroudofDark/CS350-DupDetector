447eb8cfeb0b2f942cc2095e1b8185e68f0b2144
cs_jmcfa001 (Jacob McFadden) <jmcfa001@odu.edu>
2021-11-14 07:05:03 -0500

    Implements new tests, cleaned up some old ones
    Important: Still have some classes that do not compile

--- src/test/java/edu/odu/cs/cs350/dupedetector/ReportTest.java
+++ src/test/java/edu/odu/cs/cs350/dupedetector/ReportTest.java
@@ -19,14 +19,21 @@ import java.util.*;
 class ReportTest {
 
 	//Data for SourceCodeFile until class is implemented
-	SourceCodeFile file1 = new SourceCodeFile("/home/zeil/projects/cppProject1/src/foo.cpp");
-	SourceCodeFile file2 = new SourceCodeFile("/home/zeil/projects/cppProject1/src/headers/bar.h");
-	SourceCodeFile file3 = new SourceCodeFile("H:\\cygwin\\home\\Jacob\\someCpp.cpp");
-	SourceCodeFile file4 = new SourceCodeFile("/home/fakeZeil/projects/cppProject1/src/foo.cpp");
-	SourceCodeFile file5 = new SourceCodeFile("/coolhome/fakeZeil/cppProject1/src/tool.cpp");
+	String filePath1 = "/home/zeil/projects/cppProject1/src/foo.cpp";
+	String filePath2 = "/home/zeil/projects/cppProject1/src/headers/bar.h";
+	String filePath3 = "H:\\cygwin\\home\\Jacob\\someCpp.cpp";
+	String filePath4 = "/home/fakeZeil/projects/cppProject1/src/foo.cpp";
+	String filePath5 = "/coolhome/fakeZeil/cppProject1/src/tool.cpp";
+	
+	SourceCodeFile file1 = new SourceCodeFile(filePath1);
+	SourceCodeFile file2 = new SourceCodeFile(filePath2);
+	SourceCodeFile file3 = new SourceCodeFile(filePath3);
+	SourceCodeFile file4 = new SourceCodeFile(filePath4);
+	SourceCodeFile file5 = new SourceCodeFile(filePath5);
 	
 	ArrayList<SourceCodeFile> sourceFileList1;
 	ArrayList<SourceCodeFile> sourceFileList2;
+	ArrayList<SourceCodeFile> emptySourceList;
 	
 	//Data for SuggestedRefactoring until class is implemented (tests will need to be reviewed afterwards)
 	String data1 = "/home/zeil/projects/cppProject1/src/foo.cpp:100:0\r\n"
@@ -61,6 +68,7 @@ class ReportTest {
 	ArrayList<SuggestedRefactoring> refactoringList1;
 	ArrayList<SuggestedRefactoring> refactoringList2;
 	ArrayList<SuggestedRefactoring> refactoringList3;
+	ArrayList<SuggestedRefactoring> emptyRefList;
 	
 	
 	/**
@@ -120,7 +128,14 @@ class ReportTest {
 	void testReport() {
 		Report rep = new Report(sourceFileList1, refactoringList1);
 		
-		//Check that information is as expected
+		//Check that the Source File information is correct
+		assertThat(rep.totalSourceCodeFiles(), is(3));
+		assertThat(rep.getSourceCodeFile(0), equalTo(file1));
+		assertThat(rep.getSourceCodeFile(1), equalTo(file2));
+		assertThat(rep.getSourceCodeFile(2), equalTo(file3));
+		assertThat(rep.getSourceCodeFile(1), not(equalTo(file3)));
+		
+		//Check that Refactoring information is correct
 		assertThat(rep.totalRefactorings(), is(3));
 		assertThat(rep.getRefactoring(0), equalTo(refactoring1));
 		assertThat(rep.getRefactoring(1), equalTo(refactoring2));
@@ -133,21 +148,43 @@ class ReportTest {
 		
 		//Check that Report isn't just initializing the same list despite different input
 		Report rep2 = new Report(sourceFileList1, refactoringList2);
+		Report rep3 = new Report(sourceFileList2, refactoringList1);
+		Report rep4 = new Report(sourceFileList2, refactoringList2);
 		
+		assertThat(rep2.totalSourceCodeFiles(), is(3));
 		assertThat(rep2.totalRefactorings(), is(2));
 		assertThat(rep, not(equalTo(rep2)));
 		
-		//Check that deepy copy is actually made
+		assertThat(rep3.totalSourceCodeFiles(), is(5));
+		assertThat(rep3.totalRefactorings(), is(3));
+		assertThat(rep, not(equalTo(rep3)));
+		
+		assertThat(rep4.totalSourceCodeFiles(), is(5));
+		assertThat(rep4.totalRefactorings(), is(2));
+		assertThat(rep, not(equalTo(rep4)));
+		
+		//Check that Report can handle empty lists
+		Report emptyRep = new Report(emptySourceList, emptyRefList);
+		
+		assertThat(emptyRep.totalSourceCodeFiles(), is(0));
+		assertThat(emptyRep.totalRefactorings(), is(0));
+	}
+	
+	/**
+	 * Test method for Report constructor, focus around the deep copy aspect
+	 */
+	@Test
+	void testReportDeepCopyConstructor() {
+		Report deepReport = new Report(sourceFileList1, refactoringList1);
+		
+		sourceFileList1.add(file4);
 		refactoringList1.add(refactoring4);
+		
+		assertThat(sourceFileList1.size(), is(4));
 		assertThat(refactoringList1.size(), is(4));
-		assertThat(rep.totalRefactorings(), is(3));
-		/**
-		 * Expected that since refactoringList1 is adding something new that trying to access something should
-		 * throw an error.
-		 * 
-		 * https://stackoverflow.com/questions/60289884/junit-test-indexoutofboundsexception
-		 */
-		assertThrows(IndexOutOfBoundsException.class, ()-> rep.getRefactoring(3));
+		
+		assertThat(deepReport.totalSourceCodeFiles(), is(3));
+		assertThat(deepReport.totalRefactorings(), is(3));	
 	}
 	
 	/**
@@ -161,6 +198,11 @@ class ReportTest {
 		rep.printReport(3);
 		
 		//Check that printReport didn't change things it shouldn't
+		assertThat(rep.totalSourceCodeFiles(), is(3));
+		assertThat(rep.getSourceCodeFile(0), equalTo(file1));
+		assertThat(rep.getSourceCodeFile(1), equalTo(file2));
+		assertThat(rep.getSourceCodeFile(2), equalTo(file3));
+		
 		assertThat(rep.totalRefactorings(), is(3));
 		assertThat(rep.getRefactoring(0), equalTo(refactoring1));
 		assertThat(rep.getRefactoring(1), equalTo(refactoring2));
@@ -170,6 +212,11 @@ class ReportTest {
 		String output = new String(outContent.toByteArray());
 		
 		//Assert that the output contains the expected data
+		assertThat(output, containsString(file1.getPath() + ", " + file1.getTotalTokens()));
+		assertThat(output, containsString(file2.getPath() + ", " + file2.getTotalTokens()));
+		assertThat(output, containsString(file3.getPath() + ", " + file3.getTotalTokens()));
+		assertThat(output, not(containsString(file4.getPath() + ", " + file4.getTotalTokens())));
+		
 		assertThat(output, containsString("Opportunity"));
 		assertThat(output, containsString(data1));
 		assertThat(output, containsString(data2));
@@ -180,7 +227,7 @@ class ReportTest {
 		assertThat(output, containsString("Printed 3 of 3 suggestions."));
 		
 		/**
-		 * Test Divide 1
+		 * Test Divide 1 (below tests are mainly checking the Printed suggestions section
 		 */
 		
 		//Reset the stream output to do another test
@@ -193,6 +240,11 @@ class ReportTest {
 		output = new String(outContent.toByteArray());
 		
 		//Assert that the output contains the expected data
+		assertThat(output, containsString(file1.getPath() + ", " + file1.getTotalTokens()));
+		assertThat(output, containsString(file2.getPath() + ", " + file2.getTotalTokens()));
+		assertThat(output, containsString(file3.getPath() + ", " + file3.getTotalTokens()));
+		assertThat(output, not(containsString(file4.getPath() + ", " + file4.getTotalTokens())));
+		
 		assertThat(output, containsString("Opportunity"));
 		assertThat(output, containsString(data1));
 		assertThat(output, containsString(data2));
@@ -217,6 +269,11 @@ class ReportTest {
 		output = new String(outContent.toByteArray());
 		
 		//Assert that the output contains the expected data
+		assertThat(output, containsString(file1.getPath() + ", " + file1.getTotalTokens()));
+		assertThat(output, containsString(file2.getPath() + ", " + file2.getTotalTokens()));
+		assertThat(output, containsString(file3.getPath() + ", " + file3.getTotalTokens()));
+		assertThat(output, not(containsString(file4.getPath() + ", " + file4.getTotalTokens())));
+		
 		assertThat(output, containsString("Opportunity"));
 		assertThat(output, containsString(data1));
 		assertThat(output, containsString(data2));
@@ -225,6 +282,34 @@ class ReportTest {
 		
 		//Format was grabbed from specifications, so there should be a line like this
 		assertThat(output, containsString("Printed 3 of 3 suggestions."));
+	}
+	
+	/**
+	 * Test method sortSourceFiles
+	 */
+	@Test 
+	void testsortSourceFiles() {
+		//Original list, unsorted
+		Report rep = new Report(sourceFileList2, refactoringList2);
+		//Second list to compare against
+		Report rep2 = new Report(sourceFileList2, refactoringList2);
+		
+		//Confirm equal
+		assertThat(rep2, equalTo(rep));
+		
+		//Do the sort, sorts alphabetically by absolute path (case sensitive)
+		rep2.sortSourceFiles();
+		
+		//Expected order should be file5, file4, file1, file2, file3
+		assertThat(rep2.totalSourceCodeFiles(), is(5));
+		assertThat(rep2.getSourceCodeFile(0), equalTo(file5));
+		assertThat(rep2.getSourceCodeFile(1), equalTo(file4));
+		assertThat(rep2.getSourceCodeFile(2), equalTo(file1));
+		assertThat(rep2.getSourceCodeFile(3), equalTo(file2));
+		assertThat(rep2.getSourceCodeFile(4), equalTo(file3));
+		
+		//Shouldn't be equal because ordering
+		assertThat(rep2, not(equalTo(rep)));
 	}
 	
 	/**
