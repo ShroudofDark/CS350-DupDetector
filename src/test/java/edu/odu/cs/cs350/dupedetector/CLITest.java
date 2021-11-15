27620cadf88a13b633d927b19d7a9e459315d50f
Raphael Pamie-George <rpami001@odu.edu>
2021-11-14 23:59:31 -0500

    CLI Test UPdate

--- src/test/java/edu/odu/cs/cs350/dupedetector/CLITest.java
+++ src/test/java/edu/odu/cs/cs350/dupedetector/CLITest.java
@@ -6,13 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CLITest {
     @Test void CLItest() {
-       /* CLI fileTest = new CLI();
+        CLI fileTest = new CLI();
         assertNotNull(fileTest.parseArgs(), "CLI should check property path and path index and iterate through");
         assertThat(fileTest.suppliedPaths(), ArrayList<String> supplied);
-        assertThat(fileTest.numSuggestions(), numSuggestions);
-        assertThat(fileTest.printForInvalidInvocation(), "");
-        */ 
+        assertTrue(fileTest.numSuggestions(3));
+        assertNotNull(fileTest.printForInvalidInvocation(), "\"usage: java -jar DupDetector.jar nSuggestions [ properties ] path1 [ path2 … ]\");\r\n");
+        
+        
     	
-    	//Unfortunately System Test is not running
+    	
     }
 }
