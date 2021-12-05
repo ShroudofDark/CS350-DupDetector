0144f1bfc0f8b3216c126a947850b5f2b5bca12c
Jacob McFadden <jmcfa001@odu.edu>
2021-12-05 02:55:06 -0500

    Sets Up System Tests
    Provides a bunch of not implemented systems test (and 1 implemented one)
    and some data to work alongside it

--- dupe-detector/src/stest/java/edu/odu/cs/cs350/dupedetector/TestBaseline.java
+++ dupe-detector/src/stest/java/edu/odu/cs/cs350/dupedetector/TestBaseline.java
@@ -10,6 +10,16 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Performs automated system tests by comparing output to expected out. 
+ * 
+ * Current note, can only test certain parts of the output report as suggestions
+ * are randomly defined and thus there is no consistency in what might occur.
+ * 
+ * @author Jacob
+ * @author John
+ */
+
 public final class TestBaseline {
     @Test
     void testNoArgsErrorMessage() {
@@ -21,9 +31,187 @@ public final class TestBaseline {
     @Test
     void testSystemCppPlaceholder() {
         LinkedList<String> paths = new LinkedList<String>();
-        paths.add("dupe-detector/src/test/data/cpp");
+        paths.add("src/test/data/cpp");
         String output = SysTestHelper.runJar("52", null, paths);
         assertThat(output, not(is("usage: java -jar DupDetector.jar nSuggestions [ properties ] path1 [ path2 … ]")));
     }
+    
+    /**
+     * Checks to make sure if a duplicate files added in by args doesn't get read multiple times
+     */
+    @Test
+    void testDuplicateFiles() {
+    	LinkedList<String> paths = new LinkedList<String>();
+    	paths.add("src/stest/data/test-project1/cellIndex.cpp");
+    	paths.add("src/stest/data/test-project1/cellIndex.cpp");
+    	
+    	String output = SysTestHelper.runJar("50", "src/stest/data/propertyFiles/expectedProperty.ini", paths);
+    	
+    	/**
+    	 * Either allows for checking both, helps avoid issues with different filepaths for different systems
+    	 * 
+    	 * It is important to check for the comma after given the expected output of a file report should have a comma
+    	 * after the file path is listed then the number of tokens. The file could appear again in the suggested refactoring
+    	 * report, however it won't have a comma following it. We don't care for it reappearing there.
+    	 */
+    	assertThat(output, either(containsString("src/stest/data/test-project1/cellIndex.cpp,"))
+    			.or(containsString("src\\stest\\data\\test-project1\\cellIndex.cpp,")));
+    	
+    	//Removes for / version
+    	String removedFileOutput1 = output.substring(output.indexOf("src/stest/data/test-project1/cellIndex.cpp,")+1);
+    	//Removes for \ version
+    	String removedFileOutput2 = output.substring(output.indexOf("src\\stest\\data\\test-project1\\cellIndex.cpp,")+1);
+    	
+    	//Since output1 may contain \ we have to pass this test it does
+    	assertThat(removedFileOutput1, either(not(containsString("src/stest/data/test-project1/cellIndex.cpp,")))
+    			.or(containsString("src\\stest\\data\\test-project1\\cellIndex.cpp,")));
+    	
+    	//Since output2 may contain / we have to pass this test it does
+    	assertThat(removedFileOutput2, either(containsString("src/stest/data/test-project1/cellIndex.cpp,"))
+    			.or(not(containsString("src\\stest\\data\\test-project1\\cellIndex.cpp,"))));
+    	
+    	//Providing Directory Instead
+    	paths.clear();    	
+    	paths.add("src/stest/data/test-project1/");
+    	paths.add("src/stest/data/test-project1/");
+    	
+    	output = SysTestHelper.runJar("50", "src/stest/data/propertyFiles/expectedProperty.ini", paths);
+    	
+    	assertThat(output, either(containsString("src/stest/data/test-project1/dir1/Cylinder.cpp,"))
+    			.or(containsString("src\\stest\\data\\test-project1\\dir1\\Cylinder.cpp,")));
+    	
+    	//Removes for / version
+    	removedFileOutput1 = output.substring(output.indexOf("src/stest/data/test-project1/dir1/Cylinder.cpp,")+1);
+    	//Removes for \ version
+    	removedFileOutput2 = output.substring(output.indexOf("src\\stest\\data\\test-project1\\dir1\\Cylinder.cpp,")+1);
+    	
+    	//Since output1 may contain \ we have to pass this test it does
+    	assertThat(removedFileOutput1, either(not(containsString("src/stest/data/test-project1/dir1/Cylinder.cpp,")))
+    			.or(containsString("src\\stest\\data\\test-project1\\dir1\\Cylinder.cpp,")));
+    	
+    	//Since output2 may contain / we have to pass this test it does
+    	assertThat(removedFileOutput2, either(containsString("src/stest/data/test-project1/dir1/Cylinder.cpp,"))
+    			.or(not(containsString("src\\stest\\data\\test-project1\\dir1\\Cylinder.cpp,"))));
+    }
 
+    /**
+     * Tests basic input with everything expected to be there
+     */
+    @Test
+    void testExpectedInput() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests when a provided property file is empty, using default values
+     */
+    @Test
+    void testEmptyPropertyFile() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests output with high amount of allowed numSuggestions (all eligible suggestions should print)
+     */
+    @Test
+    void testHighNumSuggestions() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests output with low amount of allowed numSuggestions (not all eligible suggestions should print)
+     */
+    @Test
+    void testLowNumSuggestions() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests output with single source file provided
+     */
+    @Test
+    void testSingleSourceFile() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests ouput with multiple directories provided
+     */
+    @Test
+    void testMultipleDirectories() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests multiple invalid property files
+     */
+    @Test
+    void testInvalidPropertyFiles() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests when multiple property files are provided, 2nd one should be read in as source file
+     */
+    @Test
+    void testMultiplePropertyFiles() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests that there is no property file provided, uses defaults
+     */
+    @Test
+    void testNoPropertyFile() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests bizzare extension choices, should still allow for them to be read in as source files
+     */
+    @Test
+    void testBizzareExtensions() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests when user only provides numSuggestions for arguments
+     */
+    @Test
+    void testNumSuggestionsOnly() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests when provided property file doesn't exist
+     */
+    @Test
+    void testPropertyFileNotFound() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests that specified files are still added even if extensions don't match directory searchers
+     */
+    @Test
+    void testIndividualFilesAddedWhenExtensionDoesNotMatchPropertyFile() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests when empty directory is provided
+     */
+    @Test
+    void testEmptyDirectory() {
+    	fail("Not Implemented");
+    }
+    
+    /**
+     * Tests when directory or file doesn't exist
+     */
+    @Test
+    void testSourceFileOrDirectoryDoesNotExist() {
+    	fail("Not Implemented");
+    }
+    
 }
