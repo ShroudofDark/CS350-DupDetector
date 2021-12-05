1d499e3717b7a902a0f04a97f1a8339143830fc9
John Hicks <jhick046@odu.edu>
2021-12-04 22:36:04 -0500

    Adds system test setup
    Skips pmd, spotbogs, and checkstyle  for both itest and stest suites.
    
    The two tests in `TestBaseline.java`
    are examples of how to use the helper methods.

---
+++ dupe-detector/src/stest/java/SysTestHelper.java
@@ -0,0 +1,73 @@
+package edu.odu.cs.cs350.dupedetector.test;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * Helper methods for system testing
+ */
+public final class SysTestHelper {
+    /**
+     * Runs the DupeDetector Jar with the specified arguments, handling (for the
+     * most part) correct CLI formatting
+     * @param nSuggestions number of suggestions to ask for
+     * @param propsFilePath props file path to use, if any
+     * @param sourcePaths either files or directories containing source code
+     * @return program output as a string
+     */
+    public static String runJar(String nSuggestions, String propsFilePath,
+        List<String> sourcePaths)
+    {
+        LinkedList<String> args = new LinkedList<String>();
+        args.add(nSuggestions);
+        if (propsFilePath != null) {
+            args.add(propsFilePath);
+        }
+        for (String path : sourcePaths) {
+            args.add(path);
+        }
+        return SysTestHelper.runJarRaw(args);
+    }
+
+    /**
+     * Runs the DupeDetector Jar with an arbitrary string as the CLI arguments  
+     * @param cliArgs. Best performance if using a LinkedList due to adding to
+     * the front of the list in this method
+     * @return
+     */
+    public static String runJarRaw(List<String> cliArgs) {
+        String jarPath = (Path.of(System.getProperty("user.dir"), "build", "libs",
+            "DupDetector.jar")).toString();
+        cliArgs.add(0, jarPath);
+        // build for program execution now
+        cliArgs.add(0, "-jar");
+        cliArgs.add(0, "java");
+        return runCommandForOutput(new ProcessBuilder(cliArgs));
+    }
+
+    // This method copied from https://stackoverflow.com/a/41292766 and modified
+    public static String runCommandForOutput(ProcessBuilder pb) {
+        Process p;
+        String result = "";
+        try {
+            p = pb.start();
+            final BufferedReader reader = new BufferedReader(
+                new InputStreamReader(p.getInputStream())
+            );
+    
+            StringJoiner sj = new StringJoiner(System.getProperty("line.separator"));
+            reader.lines().iterator().forEachRemaining(sj::add);
+            result = sj.toString();
+    
+            p.waitFor();
+            p.destroy();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return result;
+    }
+}
